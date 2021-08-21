de99be374a1ed544c80c52f56c72221a3a5abbc0
Eric Zelermyer <eric@zebomedia.com>
2021-08-20 23:42:35 -0400

    adding dismiss gesture

--- PhotoViewer/Photo Gallery/SinglePhotoViewController.swift
+++ PhotoViewer/Photo Gallery/SinglePhotoViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 final class SinglePhotoViewController: UIViewController {
+    enum Constants {
+        static let threshold: CGFloat = 80
+    }
+    
+    var dismissHandler: ((CGRect) -> Void)?
+    var dismissProgressHandler: ((CGFloat) -> Void)?
+    
     var image: UIImage? {
         didSet {
             imageView.image = image
@@ -15,6 +22,8 @@ final class SinglePhotoViewController: UIViewController {
     }
     
     private lazy var imageView: ImageZoomView = self.view.configureSubview(ImageZoomView())
+    private var dragImage: UIImageView?
+    private var dragStartPoint: CGPoint?
     
     init(image: UIImage) {
         self.image = image
@@ -32,10 +41,61 @@ final class SinglePhotoViewController: UIViewController {
         layout()
         view.backgroundColor = .clear
         imageView.image = image
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        panGesture.delegate = self
+        imageView.addGestureRecognizer(panGesture)
     }
     
     private func layout() {
         view.constrainSubview(imageView, insets: .zero)
+    }
+    
+    @objc private func handlePan(_ gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .began:
+            guard let image = imageView.image else { return }
+            let proxy = UIImageView()
+            proxy.contentMode = .scaleAspectFit
+            proxy.image = image
+            let contentSize = CGSize.aspectFit(aspectRatio: image.size, boundingSize: view.bounds.size)
+            proxy.frame = CGRect(origin: CGPoint(x: view.frame.midX - contentSize.width/2, y: view.frame.midY - contentSize.height/2), size: contentSize)
+            dragStartPoint = CGPoint(x: proxy.frame.midX, y: proxy.frame.midY)
+            imageView.isHidden = true
+            view.addSubview(proxy)
+            dragImage = proxy
+        case .changed:
+            guard let proxy = dragImage, let startPoint = dragStartPoint else { return }
+            
+            let move = gesture.translation(in: view)
+            proxy.center = CGPoint(x: startPoint.x + move.x,
+                                   y: startPoint.y + move.y)
+            let overall = abs(proxy.center.y - startPoint.y)
+            let percent = overall/Constants.threshold
+            dismissProgressHandler?(percent)
+        case .ended:
+            guard let proxy = dragImage, let startPoint = dragStartPoint else { return }
+            
+            if abs(proxy.center.y - startPoint.y) > Constants.threshold {
+                proxy.removeFromSuperview()
+                dragImage = nil
+                dismissHandler?(proxy.frame)
+            } else {
+                let animator = UIViewPropertyAnimator(duration: 0.2, curve: .easeOut) {
+                    proxy.center = startPoint
+                }
+                animator.addCompletion { [weak self] _ in
+                    proxy.removeFromSuperview()
+                    self?.dragImage = nil
+                    self?.imageView.isHidden = false
+                }
+                animator.startAnimation()
+            }
+        default:
+            guard let proxy = dragImage else { return }
+            proxy.removeFromSuperview()
+            dragImage = nil
+        }
     }
     
     func toggleZoom() {
@@ -44,5 +104,16 @@ final class SinglePhotoViewController: UIViewController {
         } else {
             imageView.setZoomScale(1, animated: true)
         }
+    }
+}
+
+extension SinglePhotoViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard let panGesture = gestureRecognizer as? UIPanGestureRecognizer else { return true }
+        
+        guard imageView.zoomScale == 1 else { return false }
+        
+        let velocity = panGesture.velocity(in: view)
+        return abs(velocity.x) < 12
     }
 }
